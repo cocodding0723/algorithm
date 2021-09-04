8c6b880c8e779f7b86c009058e1a31ce9fca0a63
cocodding0723 <46515671+cocodding0723@users.noreply.github.com>
2021-09-04 10:53:16 +0900

    optimize

--- EllerAlgorithm.cpp
+++ EllerAlgorithm.cpp
@@ -48,6 +48,7 @@ void set_row()
 
             groups.insert(make_pair(group_count, Group()));
             groups[group_count].members.push_back(&cells[cur_col][i]);
+            groups[group_count].last_rows.push_back(&cells[cur_col][i]);
 
             group_count++;
         }
@@ -72,6 +73,8 @@ void rnd_union(){
                     cells[cur_col][i + 1].left = true;
                     cells[cur_col][i].right = true;
 
+                    groups[cur_group].last_rows.push_back(&cells[cur_col][i + 1]);
+
                     flag = true;
                 }
                 break;
@@ -81,6 +84,8 @@ void rnd_union(){
                 if (i - 1 > -1 && cur_group != next_group){
                     cells[cur_col][i - 1].right = true;
                     cells[cur_col][i].left = true;
+
+                    groups[cur_group].last_rows.push_back(&cells[cur_col][i - 1]);
 
                     flag = true;
                 }
@@ -105,19 +110,26 @@ void set_cell_group(int group){
 
 void group_down(){
     unordered_map<int, Group>::iterator iter;
+    vector<Cell*> dest;
     int down_count = 0;
     int size = 0;
     int cur_row;
 
     for (iter = groups.begin(); iter != groups.end(); iter++){
         down_count = 0;
-        size = iter->second.members.size();
 
-        for (int i = 0; i < size; i++){
-            if (rand() % 2 && iter->second.members[i]->col == cur_col){
-                cur_row = iter->second.members[i]->row;
+        dest.clear();
+        dest.assign(iter->second.last_rows.begin(), iter->second.last_rows.end());
+
+        groups[iter->first].last_rows.clear();
+        // size = iter->second.members.size();
+
+        for (int i = 0; i < dest.size(); i++){
+            if (rand() % 2){
+                cur_row = dest[i]->row;
 
                 groups[iter->first].members.push_back(&cells[cur_col + 1][cur_row]);
+                groups[iter->first].last_rows.push_back(&cells[cur_col + 1][cur_row]);
 
                 cells[cur_col + 1][cur_row].up = true;
                 cells[cur_col][cur_row].down = true;
@@ -127,21 +139,16 @@ void group_down(){
             }
         }
 
-        if (down_count == 0 && size != 0){
-            for (int i = 0; i < size; i++){
-                if (iter->second.members[i]->col == cur_col){
-                    cur_row = iter->second.members[i]->row;
+        if (down_count == 0 && dest.size() != 0){
+            cur_row = iter->second.members[rand() * 100 % dest.size()]->row;
 
-                    groups[iter->first].members.push_back(&cells[cur_col + 1][cur_row]);
+            groups[iter->first].members.push_back(&cells[cur_col + 1][cur_row]);
+            groups[iter->first].last_rows.push_back(&cells[cur_col + 1][cur_row]);
 
-                    cells[cur_col + 1][cur_row].up = true;
-                    cells[cur_col][cur_row].down = true;
+            cells[cur_col + 1][cur_row].up = true;
+            cells[cur_col][cur_row].down = true;
 
-                    set_cell_group(iter->first);
-
-                    break;
-                }
-            } 
+            set_cell_group(iter->first);
         }
     }
 }
@@ -227,11 +234,12 @@ void print_maze(){
 
 int main()
 {
-    chrono::system_clock::time_point start = chrono::system_clock::now();
 
     cin >> input;
 
     srand(time(NULL));
+
+    chrono::system_clock::time_point start = chrono::system_clock::now();
 
     init();
 
@@ -245,12 +253,13 @@ int main()
 
     chrono::duration<double> sec = chrono::system_clock::now() - start;
 
-    cout << "미로 생성시 걸리는 시간 : " << sec.count() << "second" << endl;
 
     set_row();
     last_line();
 
     print_maze();
+
+    cout << "미로 생성시 걸리는 시간 : " << sec.count() << "second" << endl;
 
     return 0;
 }
